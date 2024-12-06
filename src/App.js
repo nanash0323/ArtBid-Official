42c1f4842131f822b6ff60be61da0881febc49dc
Ivan Espinosa <isespinosa@student.hau.edu.ph>
2024-12-06 22:50:11 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <main>
+      dsada
+    </main>
+    </>
+  );
+}
+
+export default App;
